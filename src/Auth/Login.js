bc62173642e5b30ef7a60c32de8990050325e58a
deepakumar-dev-creater <sean4deepak@gmail.com>
2021-07-08 09:03:05 +0530

    added full frontend vaildation
    Signed-off-by: deepakumar-dev-creater <sean4deepak@gmail.com>

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,12 +1,31 @@
 import '../index.css';
+import React from 'react';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 import // Link,
 'react-router-dom';
 
 function Login() {
+  const formik = useFormik({
+      initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().min(15, 'Maximum 15 fileds').required(),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div className="App">
       <div className="body">
-        <div className="w-3/5 p-10 bg-white shadow-xl login-container rounded-xl md:w-1/4">
+        <div className="p-10 bg-white shadow-xl login-container rounded-xl ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-10 h-10 mb-2"
@@ -23,10 +42,11 @@ function Login() {
           </svg>
           <div className="mb-5 text-2xl font-semibold">Login to continue</div>
           <form
+            onSubmit={formik.handleSubmit}
             autoComplete="off"
-            className="space-y-2 text-left"
+            className="space-y-2 text-left md:w-80"
             action=""
-            method=""
+            method="POST"
           >
             <label
               htmlFor="username"
@@ -40,7 +60,14 @@ function Login() {
               placeholder="Username"
               name="username"
               id="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
             />
+            {formik.errors.username ? (
+              <div>
+                {<p className="text-red-600">{formik.errors.username}</p>}
+              </div>
+            ) : null}
             <label
               htmlFor="Password"
               className="block text-sm font-bold text-gray-700"
@@ -53,17 +80,19 @@ function Login() {
               placeholder="Password"
               name="password"
               id="Password"
+              onChange={formik.handleChange}
+              values={formik.values.password}
             />
             <div className="p-2">
               <button
                 className="block w-1/2 h-10 mx-auto font-bold rounded-md bg-primary"
                 type="submit"
-                onSubmit={onchange()}
               >
                 Login
               </button>
             </div>
           </form>
+
           <hr className="mt-4" style={{borderBottomWidth: '5px'}} />
         </div>
       </div>
--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,12 +1,30 @@
 import '../index.css';
-
+import {useFormik} from 'formik';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import * as Yup from 'yup';
 function Register() {
+  const Formik = useFormik({
+    initialValues: {name: '', email: '', password: '', username: ''},
+    validationSchema: Yup.object({
+      name: Yup.string().strict().required('Name required'),
+      email: Yup.string().strict().email('Enter valid email').required('Email required'),
+      password: Yup.string()
+        .required('password required')
+        .min(12, 'Minimum 12 character is required'),
+      username: Yup.string()
+        .min(10, 'Minimum 10 charaters')
+        .required('username required'),
+    }),
+    onSubmit: (e) => {
+      console.warn(e);
+    },
+  });
   return (
     <div className="App">
       <div className="body">
         <div className="p-10 bg-white shadow-xl login-container rounded-xl">
-          <svg 
-          
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-10 h-10 mx-auto"
             fill="none"
@@ -20,44 +38,87 @@ function Register() {
               d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
             />
           </svg>
-          <div className="mb-5 text-2xl font-semibold text-center">Create an Account</div>
+          <div className="mb-5 text-2xl font-semibold text-center">
+            Create an Account
+          </div>
           <form
             autoComplete="off"
-            className="space-y-2 text-left md:w-96"
+            className="relative space-y-2 text-left md:w-80"
             action=""
             method="POST"
+            onSubmit={Formik.handleSubmit}
           >
-             <label
+            <label
               htmlFor="Name"
-              className="block text-sm font-bold text-gray-700"
+              className="relative block text-sm font-bold text-gray-700"
             >
               Name
+              <span className="absolute right-1 top-10">
+                {Formik.errors.name ? null : (
+                  !Formik.touched.name ? null:<FontAwesomeIcon
+                  className="text-blue-500"
+                  icon={faCheckCircle}
+                />
+                )}
+              </span>
             </label>
+
             <input
               type="text"
               className="block w-full h-10 appearance-none rounded-xl"
               placeholder="Name"
-              name="Name"
+              name="name"
               id="Name"
+              onBlur={Formik.handleBlur}
+              value={Formik.values.name}
+              onChange={Formik.handleChange}
             />
-                  <label
+
+            {Formik.errors.name && Formik.touched.name ? (
+              <p className="text-xs text-red-600">{Formik.errors.name}</p>
+            ) : null}
+            <label
               htmlFor="Email"
-              className="block text-sm font-bold text-gray-700"
+              className="relative block text-sm font-bold text-gray-700"
             >
               Email
+              <span className="absolute right-1 top-10">
+                {Formik.errors.email ? null : (
+                  !Formik.touched.email ? null:<FontAwesomeIcon
+                  className="text-blue-500"
+                  icon={faCheckCircle}
+                />
+                )}
+              </span>
             </label>
+
             <input
               type="text"
               className="block w-full h-10 appearance-none rounded-xl"
               placeholder="Email"
-              name="Email"
+              name="email"
               id="Email"
+              value={Formik.values.email}
+              onChange={Formik.handleChange}
+              onBlur={Formik.handleBlur}
             />
+            {Formik.errors.email && Formik.touched.email ? (
+              <p className="text-xs text-red-600 ">{Formik.errors.email}</p>
+            ) : null}
+
             <label
               htmlFor="username"
-              className="block text-sm font-bold text-gray-700"
+              className="relative block text-sm font-bold text-gray-700`"
             >
               User name
+              <span className="absolute right-1 top-10">
+                {Formik.errors.username ? null : (
+                   !Formik.touched.username ? null:<FontAwesomeIcon
+                   className="text-blue-500"
+                   icon={faCheckCircle}
+                 />
+                )}
+              </span>
             </label>
             <input
               type="text"
@@ -65,20 +126,42 @@ function Register() {
               placeholder="username"
               name="username"
               id="username"
+              value={Formik.values.username}
+              onChange={Formik.handleChange}
+              onBlur={Formik.handleBlur}
             />
+            {Formik.errors.username && Formik.touched.username ? (
+              <p className="text-xs text-red-600 ">{Formik.errors.username}</p>
+            ) : null}
+
             <label
               htmlFor="Password"
-              className="block text-sm font-bold text-gray-700"
+              className="relative block text-sm font-bold text-gray-700"
             >
               Password
+              <span className="absolute right-1 top-10">
+                {Formik.errors.password ? null : (
+                  !Formik.touched.password ? null:<FontAwesomeIcon
+                  className="text-blue-500"
+                  icon={faCheckCircle}
+                />
+                )}
+              </span>
             </label>
             <input
               type="password"
               className="block w-full h-10 rounded-xl"
               placeholder="Password"
-              name="Password"
+              name="password"
               id="Password"
+              value={Formik.values.password}
+              onChange={Formik.handleChange}
+              onBlur={Formik.handleBlur}
             />
+
+            {Formik.errors.password && Formik.touched.password ? (
+              <p className="text-xs text-red-600 ">{Formik.errors.password}</p>
+            ) : null}
             <div className="p-2">
               <button
                 className="block w-1/2 h-10 mx-auto font-bold rounded-md bg-primary"
@@ -86,7 +169,10 @@ function Register() {
               >
                 Register
               </button>
-              <hr className="mt-4" style={{borderBottomWidth:'5px'}}/>
+              <hr className="mt-4" style={{borderBottomWidth: '5px'}} />
+              <span className="relative right-1 top-12">
+                <i className="far fa-check-circle"></i>
+              </span>
             </div>
           </form>
         </div>
